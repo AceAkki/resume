ebeab628088b8e3e92da21bf67b7b8f854043a78
Akshay P (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2025-03-17 20:36:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ document.addEventListener("DOMContentLoaded", (fn) => {
       const downloadLink = document.createElement("a");
       if (window.href.includes("github") {
         downloadLink.href = 'https://github.com/AceAkki/resume/raw/81c76b748bc28a96c756900c3c587a12b8df90ac/res/Resume-2025.pdf';
-       consolw.log("copied")
+       console.log("copied")
       } else {
         downloadLink.href = 'res/resume-2025.pdf';
       }
