cc40df3b5ad76e85845e52041da259b0fa9c49e9
Akshay R (A-K-SHA-Y) <140274081+AceAkki@users.noreply.github.com>
2026-06-28 16:41:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", (fn) => {
-  console.log(400000 / 12);
+  //console.log(400000 / 12);
   let mainElem = document.querySelector(".main-container");
   let todayDate = new Date().toISOString().split("T")[0];
-  console.log(todayDate);
+  //console.log(todayDate);
   setTimeout((fn) => {
     let blackOverlay = document.createElement("div");
     blackOverlay.classList.add("black-overlay");
